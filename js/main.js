3316598c49faf028bcf1eb9540091a0f4d88354b
Syafiqjos <syafiqjos@gmail.com>
2021-12-06 10:36:18 +0700

    Fix operator typo on camera movement

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ function animation() {
     const topLayer = stack[stack.length - 1];
     topLayer.threejs.position[topLayer.direction] += speed;
 
-    if (camera.position.y < boxHeight * (stack.length - 2) * 4) {
+    if (camera.position.y < boxHeight * (stack.length - 2) + 4) {
         camera.position.y += speed;
     }
 
