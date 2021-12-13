1f5fb339ab3de86336f638388dcb50fd6682585c
Syafiqjos <syafiqjos@gmail.com>
2021-12-14 00:12:51 +0700

    Add skyLoader script

---
+++ js/skyLoader.js
@@ -0,0 +1,28 @@
+function loadSky(scene, url, customData) {
+	const geometry = new THREE.BoxGeometry();
+	
+	const texture = new THREE.TextureLoader().load( url );
+	const material = new THREE.MeshBasicMaterial( { map: texture } );
+
+	material.side = THREE.BackSide;
+
+	const sky = new THREE.Mesh( geometry, material );
+
+	let data = {
+		position: [0, 0, 0],
+		rotation: [0, 0, 0],
+		scale: [1, 1, 1]
+	}
+
+	data = {
+		...data,
+		...customData
+	};
+
+	// Data Modification
+	sky.position.set(...data.position);
+	sky.rotation.set(...data.rotation);
+	sky.scale.set(...data.scale);
+
+	scene.add(sky);
+}
