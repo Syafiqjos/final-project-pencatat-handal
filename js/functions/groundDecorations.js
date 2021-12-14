c6145944920130734ef3e983b299ce35f8d94b78
Syafiqjos <syafiqjos@gmail.com>
2021-12-14 13:15:23 +0700

    Update groundDecorations

--- js/functions/groundDecorations.js
+++ js/functions/groundDecorations.js
@@ -1,7 +1,11 @@
 function loadGroundDecorations() {
   const loader = new THREE.GLTFLoader();
+
+  // Ngeload mesh, bisa di copy
   loadMesh(scene, loader, 'assets/models/Gedung1.glb', {
     position: [-8, 0, 8],
+    scale: [1, 1, 1],
+    rotation: [0, 0, 0],
     materials: [
       new THREE.MeshLambertMaterial({ color: new THREE.Color(`rgb(255, 255, 255)`) })
     ],
