7dc242f6347739ba0696f9bd12a7c504c643ead9
Mohammad Tauchid <muhammadtauchid@gmail.com>
2021-12-16 04:05:07 +0700

    add interface controller

---
+++ js/interface.js
@@ -0,0 +1,23 @@
+const scoreElement = document.getElementById("score");
+const instructionsElement = document.getElementById("instructions");
+const textElement = document.getElementById("text");
+const resultsElement = document.getElementById("results");
+
+function hideMainMenu() {
+    if (instructionsElement) instructionsElement.style.display = "none";
+    if (resultsElement) resultsElement.style.display = "none";
+    if (textElement) textElement.style.display = "none";
+    if (scoreElement) {
+        scoreElement.style.zIndex = 0;
+        scoreElement.innerText = 0;
+    }
+}
+
+function showResult() {
+    if (resultsElement && !autopilot) resultsElement.style.display = "flex";
+}
+
+function hideResult() {
+    resultsElement.style.display = "none";
+    scoreElement.innerText = 0;
+}
