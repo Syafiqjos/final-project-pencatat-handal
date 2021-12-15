0c023985f52a4893794d1c561f533b92a0481184
Syafiqjos <syafiqjos@gmail.com>
2021-12-15 17:11:43 +0700

    Add tower offset env variables

--- js/env.js
+++ js/env.js
@@ -5,3 +5,7 @@ const AUTOPILOT_ERROR = 0; // float 0.0 - 1.0 -> 0 jago, 1 cupu
 
 const PLACEMENT_MODE = false; // enable birdeye view
 const PLACEMENT_MODE_HEIGHT = 120; // height of birdeye view
+
+const TOWER_OFFSET_X = -12; // main tower offset x
+const TOWER_OFFSET_Y = 0; // main tower offset y
+const TOWER_OFFSET_Z = -5; // main tower offset z
