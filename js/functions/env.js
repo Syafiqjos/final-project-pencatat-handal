2f9081b526a68a0cf861620f74fab81b0947321d
Syafiqjos <syafiqjos@gmail.com>
2021-12-15 10:09:46 +0700

    Add environment variable files

---
+++ js/functions/env.js
@@ -0,0 +1,7 @@
+const IS_ORTHOGRAPHIC = false; // if true use perspective
+const ENABLE_FOG = true; // enable fogging
+const ENABLE_AUTOPILOT = false;  // enable autoplay
+const AUTOPILOT_ERROR = 0; // float 0.0 - 1.0 -> 0 jago, 1 cupu
+
+const PLACEMENT_MODE = false; // enable birdeye view
+const PLACEMENT_MODE_HEIGHT = 120; // height of birdeye view
