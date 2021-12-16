fa12358f84384ca38af9cb101f0c080209fc4e65
Richard Asmarakandi <69513428+soraasmarakandi@users.noreply.github.com>
2021-12-16 14:49:32 +0700

    Deleting audio for maintenance

--- js/main.js
+++ js/main.js
@@ -175,22 +175,6 @@ function init() {
 }
 
 function startGame() {
-  
-  const listener = new THREE.AudioListener();
-  camera.add( listener );
-
-  // create a global audio source
-  const sound = new THREE.Audio( listener );
-
-  // load a sound and set it as the Audio object's buffer
-  const audioLoader = new THREE.AudioLoader();
-  audioLoader.load( 'assets/music/ES_Deep Down Diamond Alley - Josef Bel Habib.mp3', function( buffer ) {
-    sound.setBuffer( buffer );
-    sound.setLoop( true );
-    sound.setVolume( 0.5 );
-    sound.play();
-  });
-
   autopilot = ENABLE_AUTOPILOT || false;
   
   gameEnded = false;
